0c3fea0858803ed3769c76a52363a229cb373bf1
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-11 16:04:42 -0700

    API keys added and working

---
+++ src/config/supabaseClient.js
@@ -0,0 +1,10 @@
+// used to communicate with the supabase database
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-const Home = () => {
-  return (
-    <div className="page home">
-      <h2>Home</h2>
-    </div>
-  )
-}
+import supabase from "../config/supabaseClient";
 
-export default Home
+const Home = () => {
+	console.log(supabase);
+	return (
+		<div className="page home">
+			<h2>Home</h2>
+		</div>
+	);
+};
+
+export default Home;
