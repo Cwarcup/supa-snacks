05e8a62ccb16e612483ba25118e521d7233c5ef9
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-13 13:23:18 -0700

    react-query for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // pages
-import Home from "./pages/Home";
-import Create from "./pages/Create";
-import Update from "./pages/Update";
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Update from './pages/Update';
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
-	return (
-		<BrowserRouter>
-			<nav>
-				<h1>Supa Snacks</h1>
-				<Link to="/">Home</Link>
-				<Link to="/create">Create New Snack</Link>
-			</nav>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/create" element={<Create />} />
-				<Route path="/:id" element={<Update />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <nav>
+          <h1>Supa Snacks</h1>
+          <Link to="/">Home</Link>
+          <Link to="/create">Create New Snack</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import { SnackType } from '../types/snackType';
 import supabase from '../config/supabaseClient';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 // components
 import SnackCard from '../components/snackCard';
 
 const Home: React.FC = () => {
-  const [snacks, setSnacks] = useState<SnackType[] | null>(null);
+  // const [snacks, setSnacks] = useState<SnackType[] | null>(null);
 
-  const [fetchError, setFetchError] = useState<string | null>(null);
+  // const [fetchError, setFetchError] = useState<string | null>(null);
 
-  useEffect(() => {
-    // fetch data from supabase once
-    const fetchSnacks = async () => {
-      const { data, error } = await supabase
-        .from('snacks') // table name
-        .select(); // select all columns
+  // useEffect(() => {
+  //   // fetch data from supabase once
+  //   const fetchSnacks = async () => {
+  //     const { data, error } = await supabase
+  //       .from('snacks') // table name
+  //       .select(); // select all columns
 
-      if (error) {
-        console.log("Couldn't fetch snacks");
-        setFetchError(error.message);
-        setSnacks(null);
-      }
+  //     if (error) {
+  //       console.log("Couldn't fetch snacks");
+  //       setFetchError(error.message);
+  //       setSnacks(null);
+  //     }
 
-      if (data) {
-        setSnacks(data);
-        setFetchError(null);
-      }
-    };
+  //     if (data) {
+  //       setSnacks(data);
+  //       setFetchError(null);
+  //     }
+  //   };
 
-    fetchSnacks(); // call the function to try to fetch data
-  }, []);
+  //   fetchSnacks(); // call the function to try to fetch data
+  // }, []);
+
+  // use react-query to fetch data
+  const {
+    data: snacks,
+    isLoading,
+    isError,
+  } = useQuery(['fetchAllSnacks'], async () => {
+    const { data } = await supabase.from('snacks').select();
+    return data;
+  });
+
+  if (isLoading) {
+    return (
+      <div className="page home">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="page home">
+        <h2>Error loading the snacks</h2>
+        <p>Sorry 😢</p>
+      </div>
+    );
+  }
 
   return (
     <div className="page home">
-      {fetchError && <p className="error">{fetchError}</p>}
       {snacks && (
         <div className="snacks">
           {/* order by buttons */}
