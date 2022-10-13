fe2f21865df1c8876af2186415dc2acc5bf1657e
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-13 14:38:25 -0700

    Remove item from db

--- src/components/snackCard.tsx
+++ src/components/snackCard.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { SnackType } from '../types/snackType';
 import { Link } from 'react-router-dom';
+import supabase from '../config/supabaseClient';
 
 interface SnackCardProps {
   snack: SnackType;
 }
 
 const SnackCard: React.FC<SnackCardProps> = ({ snack }) => {
+  // handle when user clicks the delete icon
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from('snacks')
+      .delete()
+      .eq('id', snack.id) // where id = snack.id
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+  };
+
+  
+
   return (
     <div className="snack-card">
       <h3>{snack.title}</h3>
@@ -17,6 +35,9 @@ const SnackCard: React.FC<SnackCardProps> = ({ snack }) => {
         <Link to={`/update/${snack.id}`}>
           <i className="material-icons">edit</i>
         </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
       </div>
     </div>
   );
